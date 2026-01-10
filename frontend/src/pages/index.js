270ec36631cf115b30e065955da2caa9fc118f74
Matt <mtcompanies@icloud.com>
2026-01-10 18:40:14 -0500

    Default to 5 leads to save Apify

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -41,7 +41,7 @@ export default function Home() {
                 body: JSON.stringify({
                     industry: industry,
                     location: location,
-                    limit: 50
+                    limit: 5
                 })
             });
 
